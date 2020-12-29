c8565b9cde78eadb0ed1e46db814e218969b71d1
Cravez <cravezofficial@gmail.com>
2020-12-29 14:38:39 -0500

    almost done command

--- src/main/java/commands/SpawnCrate.java
+++ src/main/java/commands/SpawnCrate.java
@@ -3,9 +3,12 @@ package commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
 import cratesPackage.CratesMain;
+import net.md_5.bungee.api.ChatColor;
 
 public class SpawnCrate implements CommandExecutor{
 CratesMain plugin;
@@ -17,6 +20,12 @@ CratesMain plugin;
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if(sender instanceof Player) {
 			Player p = (Player) sender;
+			
+			ArmorStand hologram  = (ArmorStand) p.getWorld().spawnEntity(p.getLocation(), EntityType.ARMOR_STAND);
+			hologram.setVisible(false);
+			hologram.setCustomNameVisible(true);
+			hologram.setGravity(false);
+			hologram.setCustomName(ChatColor.translateAlternateColorCodes('&',"&L&1<&CMINE&1CRITICAL &CCRATES&1>"));
 		}
 		return true;
 	}
